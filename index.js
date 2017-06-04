3ac8cc4e27dea2e39ad7371f5458d34b047d3f6a
Kevin Grieger <scuffmark@gmail.com>
2017-06-04 13:32:53 -0500

    organizing functions better

--- index.js
+++ index.js
@@ -1,76 +1,12 @@
-var zzz = require('poloniex.js');
+
 var Promise = require("bluebird");
-var argv = require('yargs')
-    .usage('Usage $0 -c <currencyPair> -p <percentage> [options]')
-    .example('$0 -c BTC_ETH -p 10', 'buy 10% of your bitcoin reserves in ethereum at the lowest asking price')
-    .example('$0 -c BTC_ETH -p 10 -l 30', 'buy 10% of your bitcoin reserves in ethereum at the lowest asking price and set a limit to sell it at a price 30% higher')
-    .alias('c', 'currencyPair')
-    .alias('p', 'baseBuyPercentage')
-    .alias('l', 'sellLimitPercentage')
-    .describe('c', 'the currency pair to trade on')
-    .describe('p', 'the percentage of your base currency you want to use for purcasing')
-    .describe('l', 'the percentage higher you want to sell your currency for')
-    .describe('key', 'your poloniex api key')
-    .describe('secret', 'your poloniex secret')
-    .demand(['c','p'])
-    .help('h')
-    .alias('h', 'help')
-    .argv;
+var argv = require('./lib/setup-yargs')();
 
 var apiKey = process.env.API_KEY || argv.key;
 var secret = process.env.API_SECRET || argv.secret;
 
-var poloniex = new zzz(apiKey,secret);
+var poloniexPromise = require('./lib/poloniex-promise')(apiKey,secret);
 
-
-
-function returnTicker(){
-   return new Promise(function(resolve,reject){
-      return poloniex.returnTicker(function(err,data){
-         if(err){
-            reject(err)
-         } else {
-            resolve(data);
-         }
-      });
-   });
-}
-
-function returnBalances(){
-   return new Promise(function(resolve,reject){
-      return poloniex.returnBalances(function(err,data){
-         if(err){
-            reject(err)
-         } else {
-            resolve(data);
-         }
-      });
-   });
-}
-
-function sell(currencyA, currencyB, rate, amount){
-   return new Promise(function(resolve,reject){
-      return poloniex.sell(currencyA, currencyB, rate, amount, function(err,data){
-         if(err){
-            reject(err)
-         } else {
-            resolve(data);
-         }
-      });
-   });
-}
-
-function buy(currencyA, currencyB, rate, amount){
-   return new Promise(function(resolve,reject){
-      return poloniex.buy(currencyA, currencyB, rate, amount, function(err,data){
-         if(err){
-            reject(err)
-         } else {
-            resolve(data);
-         }
-      });
-   });
-}
 
 
 function calculateLimits(obj){
@@ -129,7 +65,7 @@ function setSellLimit(obj){
 
    console.log(obj.limits);
    var limitObj = obj.limits[currencyPair];
-   return sell(baseCurrency, tradeCurrency,limitObj.upper, limitObj.amount).then(function(data){
+   return poloniexPromise.sell(baseCurrency, tradeCurrency,limitObj.upper, limitObj.amount).then(function(data){
       console.log(data);
       return obj;
    });
@@ -140,14 +76,14 @@ function placeBuy(obj){
    var tradeCurrency = obj.tradeCurrency;
    var baseCurrency = obj.baseCurrency;
    var buyObj = obj.buys[currencyPair];
-   return buy(baseCurrency, tradeCurrency, buyObj.rate, buyObj.amount).then(function(data){
+   return poloniexPromise.buy(baseCurrency, tradeCurrency, buyObj.rate, buyObj.amount).then(function(data){
       console.log(data);
       return obj;
    });
 }
 
 function setParameters(){
-   return Promise.all([returnBalances(),returnTicker()]).then(function(data){
+   return Promise.all([poloniexPromise.returnBalances(),poloniexPromise.returnTicker()]).then(function(data){
       var obj = {};
       var currArr = argv.c.split('_');
       var baseCurrency = currArr[0].toUpperCase();
